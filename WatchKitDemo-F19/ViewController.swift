36e84543024e60a9a107d9c28b82674bea7eae86
Simranbal04 <simranbal01@gmail.com>
2019-10-15 13:10:17 -0400

    PHONE: Setup template code to send messages between phone <> watch

--- WatchKitDemo-F19/ViewController.swift
+++ WatchKitDemo-F19/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import WatchConnectivity // build in lib for making phone <> watch communicatoon
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WCSessionDelegate
+{
+    //built in methods for dealing with communication between watch <> phone
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        <#code#>
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        <#code#>
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        <#code#>
+    }
+    
+//MARK: Outlets
+    //---------
     @IBOutlet weak var counterLabel: UILabel!
 
      var phoneCounter:Int = 0
+    var messageCounter:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         print("---PHONE APP LOADED!")
@@ -23,6 +40,11 @@ class ViewController: UIViewController {
         print("PHONE BUTTON PRESSED")
                 self.phoneCounter = self.phoneCounter + 1;
                 self.counterLabel.text = "Counter:\(self.phoneCounter)"
+    }
+    
+    
+    @IBAction func sendMsgButton(_ sender: Any) {
+        print("sending message to phone")
     }
     
         
